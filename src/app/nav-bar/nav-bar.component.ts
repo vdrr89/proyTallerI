3961e8e5b0b4ee3aa17a2f82c5ff9ecb6cc6d93c
veronica romero <vdrr89@gmail.com>
2021-06-15 22:28:37 -0300

    menu en 1 renglon, sigue teniendo right en varios

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  menu = [
+    {
+      id:"home",
+      nombre: '',
+      icon:'bi bi-house-fill',
+      link:"/"
+    },
+    {
+      id:"productos",
+      nombre: 'Productos',
+      icon:'',
+      link:"/productos"
+    },
+    {
+      id:"caja",
+      nombre: '',
+      icon:'bi bi-cart-fill',
+      link:"/caja"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
